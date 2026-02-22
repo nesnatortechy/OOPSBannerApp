6696423743ba785fce0c4e7babd26f1961826708
nesnatortechy <nesnator6@gmail.com>
2026-02-22 21:08:18 +0530

    Added UC4 Banner App using Array and Loop

---
+++ UC4BannerApp.java
@@ -0,0 +1,17 @@
+public class UC4BannerApp {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+                "************************",
+                "*                      *",
+                "*    OOPS BANNER APP   *",
+                "*                      *",
+                "************************"
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
