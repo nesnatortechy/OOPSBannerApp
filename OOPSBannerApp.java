765c8d91ca2b361446c43be23a8567d9b81c3162
nesnatortechy <nesnator6@gmail.com>
2026-03-10 08:22:37 +0530

    UC1 completed: Print OOPS to Console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,10 @@
+/**
+ * Printing OOPS
+ * @author nesnatortechy
+ * @version 8
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
