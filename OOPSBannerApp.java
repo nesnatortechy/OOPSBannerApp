d8d86f714d4c0ced60517a8ed0b6b5b51d909b19
nesnatortechy <nesnator6@gmail.com>
2026-03-10 08:34:05 +0530

    UC2 completed: Print OOPS banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -3,8 +3,19 @@
  * @author nesnatortechy
  * @version 8
  */
+
+
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println("  ***     ***    ******    *****  ");
+        System.out.println(" *   *   *   *   *     *   *    * ");
+        System.out.println("*     * *     *  *     *   *     *");
+        System.out.println("*     * *     *  ******    *****  ");
+        System.out.println("*     * *     *  *         *   *  ");
+        System.out.println(" *   *   *   *   *         *    * ");
+        System.out.println("  ***     ***    *         *****  ");
+
     }
 }
