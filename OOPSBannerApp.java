acfd493b1adaba40e3b425a8cb3d1b7d8f95f09d
nesnatortechy <nesnator6@gmail.com>
2026-03-11 10:35:11 +0530

    UC6 Static Method implementation

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,10 +1,61 @@
-/**
- * Printing OOPS
- * @author nesnatortechy
- * @version 8
- */
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        String[] lines = {
+
+            String.join("   ",
+                    " ***** ",
+                    " ***** ",
+                    " ***** ",
+                    " ***** "
+            ),
+
+            String.join("   ",
+                    "*     *",
+                    "*     *",
+                    "*     *",
+                    "*      "
+            ),
+
+            String.join("   ",
+                    "*     *",
+                    "*     *",
+                    "*     *",
+                    "*      "
+            ),
+
+            String.join("   ",
+                    "*     *",
+                    "*     *",
+                    " ***** ",
+                    " ***** "
+            ),
+
+            String.join("   ",
+                    "*     *",
+                    "*     *",
+                    "*      ",
+                    "      *"
+            ),
+
+            String.join("   ",
+                    "*     *",
+                    "*     *",
+                    "*      ",
+                    "      *"
+            ),
+
+            String.join("   ",
+                    " ***** ",
+                    " ***** ",
+                    "*      ",
+                    " ***** "
+            )
+        };
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
